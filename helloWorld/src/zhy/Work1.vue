6df4aa47579e338218005a1c30a589b6c9556eca
hodiki <670756748@qq.com>
2020-10-10 17:53:34 +0800

    20.10.10

--- helloWorld/src/zhy/Work1.vue
+++ helloWorld/src/zhy/Work1.vue
@@ -23,7 +23,7 @@ export default class Work1 extends Vue {
       1,
       1000
     );
-    camera.position.z = 3;
+    camera.position.z = 50;
     // 创建一个渲染器
     interface Obj {
       canvas?: any;
@@ -37,26 +37,32 @@ export default class Work1 extends Vue {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     // 创建一个立方体
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshStandardMaterial({ emissive: 0x4a4a4a });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // const material = new THREE.MeshStandardMaterial({ emissive: 0x4a4a4a });
+    // const cube = new THREE.Mesh(geometry, material);
+    // scene.add(cube);
+
+    //创建一个圆环缓冲扭结几何体
+    const geometry = new THREE.TorusKnotBufferGeometry( 10, 3, 100, 16 );
+    const material = new THREE.MeshPhongMaterial( { color: 0xffff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
 
     //点光源
     // const light = new THREE.PointLight(0xfffff, 1, 0);
-    const light = new THREE.PointLight(0xff000, 1, 0);
-    light.position.set(0, 5, 0);
+    const light = new THREE.PointLight(0x0fff0, 1, 0);
+    light.position.set(0, 50, 0);
     scene.add(light);
 
     //网格
-    const size = 10;
+    const size = 20;
     const divisions = 8;
     const gridHelper = new THREE.GridHelper(size, divisions);
     // scene.add(gridHelper);
 
     //坐标轴
-    const axesHelper = new THREE.AxesHelper(5);
-    // scene.add(axesHelper);
+    const axesHelper = new THREE.AxesHelper(100);
+    scene.add(axesHelper);
 
     //轨道控制器
     const controls = new OrbitControls(camera, renderer.domElement);
